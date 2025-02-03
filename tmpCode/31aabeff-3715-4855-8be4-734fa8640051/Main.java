6406c348e1990057bb2f048513aa8c43b0096da9
zhoulm546811 <zhoulm546811@hundsun.com>
2025-02-04 00:59:50 +0800

    代码沙箱提交（初始化 未使用 docker）

---
+++ tmpCode/31aabeff-3715-4855-8be4-734fa8640051/Main.java
@@ -0,0 +1,8 @@
+//写一个简单的 A+B 程序
+public class Main {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println(a + b);
+    }
+}
